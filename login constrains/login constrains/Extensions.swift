6b846696a908fc8bdbec0ec67f89f0aa50fa8aa5
Abd Elrahman Atallah <abdelrahmanatallah@Abds-MacBook-Pro.local>
2024-01-02 14:21:09 +0200

    Final

---
+++ login constrains/login constrains/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  login constrains
+//
+//  Created by Abd Elrahman Atallah on 02/01/2024.
+//
+
+import UIKit
+
+extension UITextField{
+    
+    func setRightViewPasswordIcon(icon: UIImage) {
+      
+        let btnView = UIButton(frame: CGRect(x: 0, y: 0, width: ((self.frame.height) * 0.70), height: ((self.frame.height) * 0.70)))
+        btnView.setImage(icon, for: .normal)
+        btnView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5)
+        self.rightViewMode = .always
+        self.rightView = btnView
+        btnView.tintColor = .black
+        btnView.addTarget(self, action: #selector(btnTapped(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func btnTapped (sender : UIButton){
+//        print("btn tapped")
+        if sender.imageView?.image == UIImage(systemName:"eye"){
+            sender.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+            self.isSecureTextEntry = true
+        }else{
+            sender.setImage(UIImage(systemName: "eye"), for: .normal)
+            self.isSecureTextEntry = false
+        }
+    }
+    
+}
--- login constrains/login constrains/ViewController.swift
+++ login constrains/login constrains/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var passwordTF: UITextField!
     @IBOutlet var phoneTF: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tfShadow(phoneTF)
@@ -29,32 +28,6 @@ class ViewController: UIViewController {
         textField.layer.shadowRadius = 10
     }
     
+}
 
-    
-}
-extension UITextField{
-    
-    func setRightViewPasswordIcon(icon: UIImage) {
-      
-        let btnView = UIButton(frame: CGRect(x: 0, y: 0, width: ((self.frame.height) * 0.70), height: ((self.frame.height) * 0.70)))
-        btnView.setImage(icon, for: .normal)
-        btnView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 5)
-        self.rightViewMode = .always
-        self.rightView = btnView
-        
-        btnView.addTarget(self, action: #selector(btnTapped(sender:)), for: .touchUpInside)
-        
-    }
-    @objc func btnTapped (sender : UIButton){
-//        print("btn tapped")
-        if sender.imageView?.image == UIImage(systemName:"eye"){
-            sender.setImage(UIImage(systemName: "eye.slash"), for: .normal)
-            self.isSecureTextEntry = true
-        }else{
-            sender.setImage(UIImage(systemName: "eye"), for: .normal)
-            self.isSecureTextEntry = false
-        }
-    }
-    
-}
 
